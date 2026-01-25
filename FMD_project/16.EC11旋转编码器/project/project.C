cdba6920f5a72b4d4e27caeef77c785356583682
Leo-Liu-syg <986107025@qq.com>
2026-01-25 13:55:31 +0800

    EC11_移植好了数码管

---
+++ FMD_project/16.EC11旋转编码器/project/project.C
@@ -0,0 +1,127 @@
+// Project: project.prj
+//  Device: FT61F0AX
+//  Memory: Flash 10KX14b, EEPROM 128X8b, SRAM 1KX8b
+//  Author:
+// Company:
+// Version:
+//    Date:
+//===========================================================
+//===========================================================
+#include "SYSCFG.h"
+#include "FT64F0AX.h"
+#include "TDelay.h"
+#include "TM1650_IIC.h"
+//===========================================================
+
+//***********************宏定义****************************
+#define uchar unsigned char
+#define uint unsigned int
+#define ulong unsigned long
+#define TM1650_WRITE_ADDR 0x48
+#define TM1650_CMD_DISP_ON 0x01 // 8�����ȣ�7����ʾ��������ʾʹ�ܣ��ֲ�����ˣ�
+#define TM1650_CMD_ADDR_BASE 0x68
+#define SCL PB3
+#define SDA PB4
+
+// 数码管变量定义
+unsigned char seg_code[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F}; // 0~9
+unsigned char led_place[] = {0x68, 0x6A, 0x6C, 0x6E};
+
+unsigned int Number_Sum = 0;
+unsigned int Number_Ge = 0;
+unsigned int Number_Shi = 0;
+unsigned int Number_Bai = 0;
+unsigned int Number_Qian = 0;
+
+// Variable definition
+volatile char W_TMP @0x70;	 // ϵͳռ�ò�����ɾ�����޸�
+volatile char BSR_TMP @0x71; // ϵͳռ�ò�����ɾ�����޸�
+void user_isr();			 // �û��жϳ��򣬲���ɾ��
+//===========================================================
+
+//===========================================================
+// Function name��interrupt ISR
+// parameters����
+// returned value����
+//===========================================================
+void interrupt ISR(void)
+{
+#asm;			// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+	NOP;		// ϵͳ���ò�����ɾ�����޸�
+#endasm;		// ϵͳ���ò�����ɾ�����޸�
+	user_isr(); // �û��жϺ���
+}
+void user_isr() // �û��жϺ���
+{
+}
+/*-------------------------------------------------
+ * 函数名：POWER_INITIAL
+ * 功能： 	 上电系统初始化
+ * 输入：	 无
+ * 输出： 	 无
+ --------------------------------------------------*/
+void POWER_INITIAL(void)
+{
+	OSCCON = 0B01100001; // 系统时钟选择为内部振荡器8MHz,分频比为1:1
+
+	PCKEN |= 0B00000001; // AD模块时钟使能
+
+	INTCON = 0; // 禁止所有中断
+
+	PORTA = 0B00000000;
+	PORTB = 0B00000000;
+	PORTC = 0B00000000;
+
+	WPUA = 0B00000000; // 弱上拉的开关，0-关，1-开
+	WPUB = 0B00000000;
+	WPUC = 0B00000000;
+
+	WPDA = 0B00000000; // 弱下拉的开关，0-关，1-开
+	WPDB = 0B00000000;
+	WPDC = 0B00000000;
+
+	TRISA = 0B00000000; // 输入输出设置，0-输出，1-输入
+	TRISB = 0B00000010; // PB1输入
+	TRISC = 0B00000000;
+
+	PSRC0 = 0B11111111; // 源电流设置最大
+	PSRC1 = 0B11111111;
+	PSRC2 = 0B00001111;
+
+	PSINK0 = 0B11111111; // 灌电流设置最大
+	PSINK1 = 0B11111111;
+	PSINK2 = 0B00000011;
+
+	ANSELA = 0B00000000; // 设置对应的IO为数字IO
+}
+
+void main(void)
+{
+	POWER_INITIAL(); // �0�3�0�1�1�7�1�7�0�3�1�7�1�7
+	TM1650_Init();
+	TM1650_Set(led_place[0], seg_code[9]); // 能显示，测试
+	while (1)
+	{
+	}
+}
+//===========================================================
